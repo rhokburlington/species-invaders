e4062f0de7838bc53de0f8285bc34c3ec0942b38
Scott Markoski <s.markoski@gmail.com>
2012-06-02 14:19:05 -0400

    Added the species by id api call

--- App/Models/species.model.php
+++ App/Models/species.model.php
@@ -3,7 +3,25 @@
 class species extends CoreModel {
 	
 	protected $speciesid;
+	protected $kingdom;
+	protected $phylum;
+	protected $class;
+	protected $order;
+	protected $family;
+	protected $genus;
+	protected $species;
+	protected $extra_notes;
+	protected $commonNames; //array of common name objects
 	
+	
+	public function getSpeciesByID($id) {
+		$db = DB::instance();
+		$query = 'SELECT * FROM species WHERE speciesid=?';
+		$db->query($query, 'i', array($id));
+		if ($resultRow = $db->fetchResult()) {
+			$this->populate($resultRow);
+		}
+	}
 	
 }
 

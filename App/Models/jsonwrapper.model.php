4aa028ab0458a6754ebefd06e37ea7f6ada33db4
Scott Markoski <s.markoski@gmail.com>
2012-06-02 16:23:21 -0400

    New json encoding stuff

--- App/Models/jsonwrapper.model.php
+++ App/Models/jsonwrapper.model.php
@@ -3,7 +3,19 @@
 class jsonwrapper extends CoreModel {
 	
 	public function json() {
-		return json_encode(get_object_vars($this));	
+		$objectVars = get_object_vars($this);
+		foreach ($objectVars as &$objvar) {
+			if (is_array($objvar)) {
+				foreach ($objvar as &$o) {
+					if ($o instanceof self) {
+						$o = $o->json();
+					}
+				}
+			} else if ($objvar instanceof self)  {
+				$objvar = $objvar->json();
+			}
+		}
+		return json_encode($objectVars);	
 	}
 	
 }

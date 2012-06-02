b12d568bac549168d35c389bf69b97a84b50a8c6
Scott Markoski <s.markoski@gmail.com>
2012-06-02 16:36:32 -0400

    hopefully the final change the the json encoder

--- App/Models/jsonwrapper.model.php
+++ App/Models/jsonwrapper.model.php
@@ -3,19 +3,23 @@
 class jsonwrapper extends CoreModel {
 	
 	public function json() {
+		return json_encode($this->toArray());	
+	}
+	
+	public function toArray() {
 		$objectVars = get_object_vars($this);
 		foreach ($objectVars as &$objvar) {
 			if (is_array($objvar)) {
 				foreach ($objvar as &$o) {
-					if (is_object($o)) {
-						$o = get_object_vars($o);
+					if (is_object($o) && $o instanceof self) {
+						$o = $o->toArray($o);
 					}
 				}
-			} else if (is_object($objvar))  {
-				$objvar = get_object_vars($o);
+			} else if (is_object($objvar) && $objvar instanceof self)  {
+				$objvar = $objvar->toArray();
 			}
 		}
-		return json_encode($objectVars);	
+		return $objVars;
 	}
 	
 }

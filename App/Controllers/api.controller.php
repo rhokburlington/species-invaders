5165713ff9c5181798fde754f26d10cb497e6d03
Scott Markoski <s.markoski@gmail.com>
2012-06-03 09:54:41 -0400

    Added the insert db query for locations

--- App/Controllers/api.controller.php
+++ App/Controllers/api.controller.php
@@ -57,8 +57,6 @@ class api extends CoreController implements ICoreController {
 				break;
 			case null:
 				try {
-					error_log(print_r($_POST, TRUE));
-					$location->formatPolygon(Input::post('polygon'));
 					$result = json_encode($location->insertNewLocation(Input::post('name'), Input::post('polygon')));
 				} catch (InputIOException $e) {
 					$result = json_encode($location->getAllLocations());
